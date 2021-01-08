932b059a6d18e4c77c244da29772eae2ec62c347
Yuvraj Sreepathi <uvsre@macbook-pro.lan>
2021-01-08 17:04:34 -0500

    Init commit

---
+++ src/app.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
+const { json } = require('express')
+
+const app = express()
+
+//Define paths for express configs
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+//Load a dynamic page
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+        name: 'Yuvraj Sreepathi'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Yuvraj Sreepathi'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is the help page.',
+        title: 'Help Page',
+        name: 'Yuvraj Sreepathi'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error:'You must provide an address!'
+        })
+    }
+    
+    geocode(req.query.address, (error, { latitude, longitude, location } ={}) => {
+        if(error){
+            return res.send({ error})
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({ error })
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: 'Help 404 Page',
+        name: 'Yuvraj Sreepathi',
+        errorMessage: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Page',
+        name: 'Yuvraj Sreepathi',
+        errorMessage: 'Page not found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up.')
+})
+
+
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+
+const forecast = (latitude, longitude, callback) =>{
+    const url = 'http://api.weatherstack.com/current?access_key=ec4f18182a02c9ff64f7b70efa96ea3e&query=' + 
+    longitude + ',' + latitude + '&units=f'  
+
+    request({ url, json: true}, (error, { body }) => {
+        if(error){
+            callback('Unable to connect to weather services!', undefined)
+        }
+        else if(body.error){
+            callback('Invalid coordinates. Try another search.', undefined)
+        }
+        else{
+            callback(undefined, body.current.weather_descriptions[0] + 
+                 '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' 
+                 + body.current.feelslike + ' degrees.')
+        }
+    })
+}
+
+module.exports = forecast
